2f2e057b669fef7e503ed5024614c5cd6dbab328
shaniya <shaniyasharma17@gmail.com>
2026-08-18 18:06:03 +0530

    pcont

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ThemeProvider } from './context/Themecontext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
